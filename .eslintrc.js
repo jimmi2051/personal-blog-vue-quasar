0d5fa166d5ded83ff79b7991d70030470e2ba236
Thanh Ly <thanh.ngly@kyanon.digital>
2020-04-01 00:10:44 +0700

    [Update] Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
   },
   extends: ["plugin:vue/strongly-recommended", "eslint:recommended"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": "off",
+    "no-debugger": "off",
     "vue/no-unused-vars": "off",
     "vue/max-attributes-per-line": "off",
     "vue/no-unused-components": "off",

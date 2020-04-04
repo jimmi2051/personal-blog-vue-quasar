66344a5d53e66276a676b43948884c20ee01d56f
Thanh Ly <thanh.ngly@kyanon.digital>
2020-04-05 02:34:47 +0700

    [Fix] Footer Responsive

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,29 +1,29 @@
 <template>
-  <q-footer elevated>
-    <q-toolbar class="bg-black">
-      <div class="text-subtitle1">© 2020 Nguyen Ly Thanh. All rights reserved.</div>
-      <q-space />
-      <div class="text-subtitle1">
-        Tel:
-        <q-btn
-          class="no-padding"
-          type="a"
-          href="tel:0569363575"
-          no-shadow
-          target="__blank"
-        >(+84) 569363575</q-btn>
-      </div>
-      <q-separator dark vertical class="q-ml-sm q-mr-sm" inset />
-      <div class="text-subtitle1">
-        E-Mail:
-        <q-btn
-          class="no-padding"
-          type="a"
-          href="mailto:thanhnl0697@gmail.com"
-          no-shadow
-          target="__blank"
-        >thanhnl0697@gmail.com</q-btn>
-      </div>
-    </q-toolbar>
-  </q-footer>
+  <div class="bg-black row footer justify-center">
+    <div
+      class="footer-title col-xs-12 col-sm-12 col-md-4"
+    >© 2020 Nguyen Ly Thanh. All rights reserved.</div>
+    <!-- <q-space /> -->
+    <div class="footer-title col-xs-12 col-sm-6 col-md-4">
+      Tel:
+      <q-btn
+        class="no-padding"
+        type="a"
+        href="tel:0569363575"
+        no-shadow
+        target="__blank"
+      >(+84) 569363575</q-btn>
+    </div>
+    <!-- <q-separator dark vertical class="q-ml-sm q-mr-sm" inset /> -->
+    <div class="footer-title col-xs-12 col-sm-6 col-md-4">
+      E-Mail:
+      <q-btn
+        class="no-padding"
+        type="a"
+        href="mailto:thanhnl0697@gmail.com"
+        no-shadow
+        target="__blank"
+      >thanhnl0697@gmail.com</q-btn>
+    </div>
+  </div>
 </template>

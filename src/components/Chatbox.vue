3c844649f96d808c75c8c5f4215f9e3edd4e014d
Thanh Ly <thanh.ngly@kyanon.digital>
2020-11-17 12:07:17 +0700

    [Update] Messenger

--- src/components/Chatbox.vue
+++ src/components/Chatbox.vue
@@ -1,32 +1,78 @@
 <template>
-  <div class="message-box">
-    <div class="message-box__header"></div>
-    <div class="message-box__body">
-      <div v-if="messages.length">
-        <div
-          v-for="message in messages"
-          :key="message.message"
-          style="width: 100%; max-width: 400px"
-        >
-          <q-chat-message
-            :name="message.user ? message.user : ''"
-            :text="[message.message]"
-            :sent="message.sent ? true : false"
-            :bg-color="message.sent ? 'amber-7' : 'primary'"
+  <div>
+    <div :class="isShow ? `show-message` : `show-message is-show`">
+      <button @click="showMessage" type="button" class="show-message__btn">
+        <i class="fab fa-facebook-messenger fa-2x" aria-hidden="true" />
+      </button>
+    </div>
+    <q-dialog v-model="prompt" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">Your name</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input
+            dense
+            v-model="name"
+            autofocus
+            @keyup.enter="prompt = false"
           />
+        </q-card-section>
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancel" v-close-popup />
+          <q-btn
+            flat
+            label="Submit name"
+            @click="isShow = true"
+            v-close-popup
+          />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <div :class="isShow ? `message-box is-show` : `message-box`">
+      <div class="message-box__header">
+        <q-avatar>
+          <img
+            src="https://cdn.quasar.dev/img/avatar1.jpg"
+            class="avatar-img"
+          />
+        </q-avatar>
+        <p class="name-user">{{ name }}</p>
+        <button @click="isShow = false" type="button" class="btn-remove">
+          <i class="fa fa-times" />
+        </button>
+      </div>
+      <div class="message-box__body" id="message-box__body">
+        <div v-if="messages.length">
+          <div
+            v-for="(message, index) in messages"
+            :key="index"
+            style="width: 100%; max-width: 400px"
+          >
+            <q-chat-message
+              :name="message.user ? message.user : ''"
+              :text="[message.message]"
+              :sent="message.sent ? true : false"
+              :text-color="message.sent ? 'black' : 'white'"
+              :bg-color="message.sent ? 'amber-7' : 'light-blue'"
+            />
+          </div>
         </div>
       </div>
-    </div>
-    <div class="message-box__footer">
-      <input
-        v-model="msgToSend"
-        class="input-chat"
-        type="text"
-        placeholder="Aa"
-      />
-      <button v-on:click="onSendMessage" class="btn-send">
-        Send
-      </button>
+      <div class="message-box__footer">
+        <input
+          v-model="msgToSend"
+          class="input-chat"
+          type="text"
+          placeholder="Aa"
+          @keyup.enter="onSendMessage"
+        />
+        <button type="button" @click="onSendMessage" class="btn-send">
+          <i class="fas fa-paper-plane" />
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -36,7 +82,7 @@ import { isEmpty } from "lodash";
 import FetchApi from "utils/FetchApi";
 import { CHANNEL } from "utils/Constants";
 import { v4 as uuidv4 } from "uuid";
-const names = ["Tí", "Tèo", "Tủn"];
+
 export default {
   created: function() {
     // console.log("this ==>", this.$ref);
@@ -51,6 +97,12 @@ export default {
     channel.bind("chat-message", data => {
       let id = localStorage.getItem("userId");
       if (id !== data.id) {
+        if (this.messages.length > 0) {
+          const lastestMsg = this.messages[this.messages.length - 1];
+          if (lastestMsg.id === data.id) {
+            delete data.user;
+          }
+        }
         this.messages.push(data);
       }
     });
@@ -58,19 +110,27 @@ export default {
   data() {
     return {
       messages: [],
-      msgToSend: ""
+      msgToSend: "",
+      isShow: false,
+      prompt: false,
+      name: ""
     };
   },
   methods: {
     onSendMessage() {
+      if (this.msgToSend === "") {
+        this.$q.notify({
+          message: "Oops! Sorry, please enter your message.",
+          color: "light-blue",
+          icon: "announcement"
+        });
+        return;
+      }
       let id = localStorage.getItem("userId");
-      let name = localStorage.getItem("userName");
       if (isEmpty(id)) {
-        const index = Math.floor(Math.random() * 3);
         id = uuidv4();
-        name = names[index];
         localStorage.setItem("userId", id);
-        localStorage.setItem("userName", name);
+        localStorage.setItem("userName", this.name);
       }
 
       const message = {
@@ -79,13 +139,19 @@ export default {
         message: this.msgToSend,
         sent: true
       };
+      if (this.messages.length > 0) {
+        const lastestMsg = this.messages[this.messages.length - 1];
+        if (lastestMsg.id === message.id) {
+          delete message.user;
+        }
+      }
       this.messages.push(message);
 
       const payload = {
         uri: "send-message",
         params: {
           id,
-          user: name,
+          user: this.name,
           message: this.msgToSend,
           channel: CHANNEL
         },
@@ -95,9 +161,49 @@ export default {
       };
       FetchApi(payload).then(response => {
         // this.messages.push(response);>
-        console.log("RES ==>", response);
+        if (response.id) {
+          this.msgToSend = "";
+        } else {
+          this.$q.notify({
+            message:
+              "Oops! Sorry, can't send message now. Please wait a few minutes and try again. Thanks",
+            color: "light-blue",
+            icon: "announcement"
+          });
+        }
       });
+    },
+    showMessage() {
+      if (isEmpty(this.name)) {
+        let id = localStorage.getItem("userId");
+        let name = localStorage.getItem("userName");
+        if (isEmpty(id)) {
+          // const index = Math.floor(Math.random() * 3);
+          // id = uuidv4();
+          // name = names[index];
+          // localStorage.setItem("userId", id);
+          // localStorage.setItem("userName", name);
+          this.prompt = true;
+        } else {
+          this.name = name;
+          this.isShow = true;
+        }
+      } else {
+        this.isShow = true;
+      }
+    },
+    scrollToEnd() {
+      const container = this.$el.querySelector("#message-box__body");
+      container.scrollTop = container.scrollHeight - container.clientHeight;
     }
+  },
+  updated() {
+    // This will be called when the component updates
+    // try toggling a todo
+    this.scrollToEnd();
+  },
+  mounted() {
+    this.scrollToEnd();
   }
 };
 </script>

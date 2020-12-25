740e82bb8396c3a8bae635822a8d2233a95942c9
Thanh Ly <thanh.ngly@kyanon.digital>
2020-12-26 00:29:30 +0700

    [Add] Sitemap

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,37 @@
 const path = require("path");
+const SitemapPlugin = require("sitemap-webpack-plugin").default;
+const paths = [
+  {
+    path: "/",
+    lastmod: new Date().toISOString().slice(0, 10),
+    priority: 0.8,
+    changefreq: "hourly"
+  },
+  {
+    path: "/about",
+    lastmod: new Date().toISOString().slice(0, 10),
+    priority: 0.7,
+    changefreq: "hourly"
+  }
+];
+
 module.exports = {
   configureWebpack: {
     resolve: {
       modules: [path.resolve("./src"), path.resolve("./node_modules")]
-    }
+    },
+    plugins: [
+      new SitemapPlugin({
+        base: "https://deftnt-blog.herokuapp.com",
+        paths,
+        options: {
+          filename: "sitemap.xml",
+          lastmod: true,
+          changefreq: "hourly",
+          priority: 0.8
+        }
+      })
+    ]
   },
 
   pluginOptions: {
